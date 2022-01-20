c493b57902fed249f7348d2cb333a9f512bdeb11
Илья Воронцов <IlyaVorWork2408@gmail.com>
2022-01-20 16:48:35 +0700

    inital commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css"
 const Home: NextPage = () => {
   const tryRequest = () => {
     let request = fetch(
-      "https://api.vk.com/method/users.get?user_ids=yareyaredazzle&fields=bdate&access_token=e7811478e7811478e781147863e7fbf90bee781e781147886680247f29066391477cac0&v=5.131"
+      "https://api.vk.com/method/users.get?user_ids=yareyaredazze&fields=bdate&access_token=9Kzcs7NW8vQIOQVatZkJ&v=5.131"
     )
     console.log(request)
   }
