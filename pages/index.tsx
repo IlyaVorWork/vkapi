5aa0dc3f29940ed55fbe9c294b2614997f463656
Илья Воронцов <IlyaVorWork2408@gmail.com>
2022-01-20 16:33:01 +0700

    inital commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import type { NextPage } from "next"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
-  let request = fetch(
-    "https://api.vk.com/method/users.get?user_ids=yareyaredazzle&fields=bdate&access_token=e7811478e7811478e781147863e7fbf90bee781e781147886680247f29066391477cac0&v=5.131"
+  const tryRequest = () => {
+    let request = fetch(
+      "https://api.vk.com/method/users.get?user_ids=yareyaredazzle&fields=bdate&access_token=e7811478e7811478e781147863e7fbf90bee781e781147886680247f29066391477cac0&v=5.131"
+    )
+    console.log(request)
+  }
+
+  return (
+    <div className={styles.container}>
+      <button onClick={() => tryRequest}>Вк апи</button>
+    </div>
   )
-
-  console.log(request)
-
-  return <div className={styles.container}>Вк апи</div>
 }
 
 export default Home
