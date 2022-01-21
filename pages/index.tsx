fdb5901c07148d06aefed7d71b05c1718dbbefd0
Илья Воронцов <IlyaVorWork2408@gmail.com>
2022-01-21 22:03:54 +0700

    i

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,9 +52,9 @@ const Home: NextPage = () => {
           "e7811478e7811478e781147863e7fbf90bee781e781147886680247f29066391477cac0",
         v: "5.131",
       },
-      (res) => {
-        console.log(res)
-        setUser({ ...user, avatar: res.result.photo_400_orig })
+      (response) => {
+        console.log(response)
+        setUser({ ...user, avatar: response.result.photo_400_orig })
       }
     )
     /*setUser(status.session.user)
